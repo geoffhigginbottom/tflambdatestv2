c81457d1b49e9b7d7c01d31d13b419734557a461
Geoff Higginbottom <github@geoffh.co.uk>
2020-11-20 14:06:52 +0000

    Added new variable generation code

--- api_gateway_retailorderprice.tf
+++ api_gateway_retailorderprice.tf
@@ -1,7 +1,25 @@
 ### API Gateway ###
-## Name has the same string appended to it as applied
-## to the name of the associated function.
 resource "aws_api_gateway_rest_api" "retailorderprice" {
   count       = var.function_count
-  name        = "Retail_Order_Price_${element(var.function_ids, count.index)}_api_gateway"
+  name        = "RetailOrderPrice_${element(var.function_ids, count.index)}_api_gateway"
+}
+
+### Append name of RetailOrderPrice function name to the invoke url and store in aws_ssm
+### so we can retrieve it within function_retailorder.
+### Using ssm_paramater as we are using count so cannot use outputs.
+resource "aws_ssm_parameter" "retailorderprice_invoke_url" {
+  count = var.function_count
+  name  = "retailorderprice_invoke_url_${element(var.function_ids, count.index)}"
+  type  = "String"
+  value = join("/",[aws_api_gateway_deployment.retailorderprice[count.index].invoke_url, aws_lambda_function.retailorderprice[count.index].function_name])
+  overwrite = true
+}
+
+### Debugging Outputs
+output "retailorderprice_invoke_url" {
+  value =  formatlist(
+    "%s, %s", 
+    aws_api_gateway_rest_api.retailorderprice.*.name,
+    aws_ssm_parameter.retailorderprice_invoke_url.*.value
+  )
 }
