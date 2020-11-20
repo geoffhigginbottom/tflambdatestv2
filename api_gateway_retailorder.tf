9f445439d50862790a2496609616797baca08746
Geoff Higginbottom <github@geoffh.co.uk>
2020-11-20 14:06:13 +0000

    Renamed

--- api_gateway_retailorder.tf
+++ api_gateway_retailorder.tf
@@ -1,7 +1,5 @@
 ### API Gateway ###
-## Name has the same string appended to it as applied
-## to the name of the associated function.
 resource "aws_api_gateway_rest_api" "retailorder" {
   count       = var.function_count
-  name        = "Retail_Order_${element(var.function_ids, count.index)}_api_gateway"
+  name        = "RetailOrder_${element(var.function_ids, count.index)}_api_gateway"
 }
