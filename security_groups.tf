32b48dd9aa7fa67dd9a219cb5b7e68cfd10912aa
Geoff Higginbottom <github@geoffh.co.uk>
2020-11-25 14:06:31 +0000

    Create security_groups.tf

---
+++ security_groups.tf
@@ -0,0 +1,52 @@
+resource "aws_security_group" "otc" {
+  name = "Splunk-Open-Telemetry-Collector"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    from_port   = 8888
+    to_port     = 8888
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    from_port   = 6501
+    to_port     = 6501
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
