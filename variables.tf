50cd695a21b765496e0709ffcf023554f03e8b19
Geoff Higginbottom <github@geoffh.co.uk>
2020-11-25 17:58:36 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,6 +4,10 @@ variable "key_name" {
   default = []
 }
 
+variable "private_key_path"{
+  default = []
+}
+
 variable "instance_type" {
   default = []
 }
@@ -13,7 +17,7 @@ variable "profile" {
 }
 
 variable "region" {
-  description = "Select region (1:eu-west-1, 2:eu-west-3, 3:us-east-1, 4:us-east-2, 5:us-west-1, 6:us-west-2, 7:ap-southeast-1, 8:ap-southeast-2, 9:sa-east-1 )"
+  description = "Select region (1:eu-west-1, 2:eu-west-3, 3:eu-central-1, 4:us-east-1, 5:us-east-2, 6:us-west-1, 7:us-west-2, 8:ap-southeast-1, 9:ap-southeast-2, 10:sa-east-1 )"
 }
 
 variable "aws_region" {
@@ -21,41 +25,46 @@ variable "aws_region" {
     default = {
       "1" = "eu-west-1"
       "2" = "eu-west-3"
-      "3" = "us-east-1"
-      "4" = "us-east-2"
-      "5" = "us-west-1"
-      "6" = "us-west-2"
-      "7" = "ap-southeast-1"
-      "8" = "ap-southeast-2"
-      "9" = "sa-east-1"
+      "3" = "eu-central-1"
+      "4" = "us-east-1"
+      "5" = "us-east-2"
+      "6" = "us-west-1"
+      "7" = "us-west-2"
+      "8" = "ap-southeast-1"
+      "9" = "ap-southeast-2"
+      "10" = "sa-east-1"
     }
 }
 
+## List available at https://github.com/signalfx/lambda-layer-versions/blob/master/python/PYTHON.md ##
 variable "region_wrapper_python" {
   default = {
     "1" = "arn:aws:lambda:eu-west-1:254067382080:layer:signalfx-lambda-python-wrapper:11"
     "2" = "arn:aws:lambda:eu-west-3:254067382080:layer:signalfx-lambda-python-wrapper:11"
-    "3" = "arn:aws:lambda:us-east-1:254067382080:layer:signalfx-lambda-python-wrapper:11"
-    "4" = "arn:aws:lambda:us-east-2:254067382080:layer:signalfx-lambda-python-wrapper:11"
-    "5" = "arn:aws:lambda:us-west-1:254067382080:layer:signalfx-lambda-python-wrapper:11"
-    "6" = "arn:aws:lambda:us-west-2:254067382080:layer:signalfx-lambda-python-wrapper:11"
-    "7" = "arn:aws:lambda:ap-southeast-1:254067382080:layer:signalfx-lambda-python-wrapper:11"
-    "8" = "arn:aws:lambda:ap-southeast-2:254067382080:layer:signalfx-lambda-python-wrapper:11"
-    "9" = "arn:aws:lambda:sa-east-1:254067382080:layer:signalfx-lambda-python-wrapper:11"  
+    "3" = "arn:aws:lambda:eu-central-1:254067382080:layer:signalfx-lambda-python-wrapper:11"
+    "4" = "arn:aws:lambda:us-east-1:254067382080:layer:signalfx-lambda-python-wrapper:11"
+    "5" = "arn:aws:lambda:us-east-2:254067382080:layer:signalfx-lambda-python-wrapper:11"
+    "6" = "arn:aws:lambda:us-west-1:254067382080:layer:signalfx-lambda-python-wrapper:11"
+    "7" = "arn:aws:lambda:us-west-2:254067382080:layer:signalfx-lambda-python-wrapper:11"
+    "8" = "arn:aws:lambda:ap-southeast-1:254067382080:layer:signalfx-lambda-python-wrapper:11"
+    "9" = "arn:aws:lambda:ap-southeast-2:254067382080:layer:signalfx-lambda-python-wrapper:11"
+    "10" = "arn:aws:lambda:sa-east-1:254067382080:layer:signalfx-lambda-python-wrapper:11"  
   }
 }
 
+## List available at https://github.com/signalfx/lambda-layer-versions/blob/master/node/NODE.md ##
 variable "region_wrapper_nodejs" {
   default = {
-    "1" = "arn:aws:lambda:eu-west-1:254067382080:layer:signalfx-lambda-nodejs-wrapper:17"
-    "2" = "arn:aws:lambda:eu-west-3:254067382080:layer:signalfx-lambda-nodejs-wrapper:17"
-    "3" = "arn:aws:lambda:us-east-1:254067382080:layer:signalfx-lambda-nodejs-wrapper:18"
-    "4" = "arn:aws:lambda:us-east-2:254067382080:layer:signalfx-lambda-nodejs-wrapper:18"
-    "5" = "arn:aws:lambda:us-west-1:254067382080:layer:signalfx-lambda-nodejs-wrapper:18"
-    "6" = "arn:aws:lambda:us-west-2:254067382080:layer:signalfx-lambda-nodejs-wrapper:18"
-    "7" = "arn:aws:lambda:ap-southeast-1:254067382080:layer:signalfx-lambda-nodejs-wrapper:17"
-    "8" = "arn:aws:lambda:ap-southeast-2:254067382080:layer:signalfx-lambda-nodejs-wrapper:17"
-    "9" = "arn:aws:lambda:sa-east-1:254067382080:layer:signalfx-lambda-nodejs-wrapper:17"  
+    "1" = "arn:aws:lambda:eu-west-1:254067382080:layer:signalfx-lambda-nodejs-wrapper:18"
+    "2" = "arn:aws:lambda:eu-west-3:254067382080:layer:signalfx-lambda-nodejs-wrapper:18"
+    "3" = "arn:aws:lambda:eu-central-1:254067382080:layer:signalfx-lambda-nodejs-wrapper:19"
+    "4" = "arn:aws:lambda:us-east-1:254067382080:layer:signalfx-lambda-nodejs-wrapper:18"
+    "5" = "arn:aws:lambda:us-east-2:254067382080:layer:signalfx-lambda-nodejs-wrapper:18"
+    "6" = "arn:aws:lambda:us-west-1:254067382080:layer:signalfx-lambda-nodejs-wrapper:18"
+    "7" = "arn:aws:lambda:us-west-2:254067382080:layer:signalfx-lambda-nodejs-wrapper:18"
+    "8" = "arn:aws:lambda:ap-southeast-1:254067382080:layer:signalfx-lambda-nodejs-wrapper:17"
+    "9" = "arn:aws:lambda:ap-southeast-2:254067382080:layer:signalfx-lambda-nodejs-wrapper:17"
+    "10" = "arn:aws:lambda:sa-east-1:254067382080:layer:signalfx-lambda-nodejs-wrapper:17"  
   }
 }
 
@@ -149,22 +158,26 @@ variable "collector_image" {
 
 ### Function URLs ###
 
-# variable "function_example_url" {
-#   default = "https://raw.githubusercontent.com/geoffhigginbottom/lambda_functions/main/example/main.js"
-# }
-
 variable "function_retailorder_url" {
-  default = "https://raw.githubusercontent.com/geoffhigginbottom/lambda_functions/main/RetailOrder/APM/Lambda_Function.py"
+  # default = "https://raw.githubusercontent.com/p-hagen-Signalfx/SplunkLambdaAPM/master/Lambdas/APM/RetailOrderAPM/Lambda_Function.py"
+  default = "https://raw.githubusercontent.com/p-hagen-Signalfx/SplunkLambdaAPM/master/Lambdas/Base/RetailOrder/Lambda_Function.py"
 }
 
 variable "function_retailorderline_url" {
-  default = "https://raw.githubusercontent.com/geoffhigginbottom/lambda_functions/main/RetailOrderLine/APM/Lambda_Function.py"
+  # default = "https://raw.githubusercontent.com/p-hagen-Signalfx/SplunkLambdaAPM/master/Lambdas/APM/RetailOrderLineAPM/Lambda_Function.py"
+  default = "https://raw.githubusercontent.com/p-hagen-Signalfx/SplunkLambdaAPM/master/Lambdas/Base/RetailOrderLine/Lambda_Function.py"
 }
 
 variable "function_retailorderprice_url" {
-  default = "https://raw.githubusercontent.com/geoffhigginbottom/lambda_functions/main/RetailOrderPrice/APM/index.js"
+  # default = "https://raw.githubusercontent.com/p-hagen-Signalfx/SplunkLambdaAPM/master/Lambdas/APM/RetailOrderPriceAPM/index.js"
+  default = "https://raw.githubusercontent.com/p-hagen-Signalfx/SplunkLambdaAPM/master/Lambdas/Base/RetailOrderPrice/index.js"
 }
 
 variable "function_retailorderdiscount_url" {
-  default = "https://raw.githubusercontent.com/geoffhigginbottom/lambda_functions/main/RetailDiscount/APM/index.js"
+  # default = "https://raw.githubusercontent.com/p-hagen-Signalfx/SplunkLambdaAPM/master/Lambdas/APM/RetailDiscountAPM/index.js"
+  default = "https://raw.githubusercontent.com/p-hagen-Signalfx/SplunkLambdaAPM/master/Lambdas/Base/RetailDiscount/index.js"
+}
+
+variable "java_app_url" {
+  default = "https://github.com/p-hagen-Signalfx/SplunkLambdaAPM.git"
 }
