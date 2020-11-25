ea279ea2b1f82084dd7a69471df62a40b4bf78b4
Geoff Higginbottom <github@geoffh.co.uk>
2020-11-25 17:57:56 +0000

    Update api_gateway_retailorder.tf

--- api_gateway_retailorder.tf
+++ api_gateway_retailorder.tf
@@ -3,3 +3,21 @@ resource "aws_api_gateway_rest_api" "retailorder" {
   count       = var.function_count
   name        = "RetailOrder_${element(var.function_ids, count.index)}_api_gateway"
 }
+
+
+resource "aws_ssm_parameter" "retailorder_invoke_url" {
+  count = var.function_count
+  name  = "retailorder_invoke_url_${element(var.function_ids, count.index)}"
+  type  = "String"
+  value = aws_api_gateway_deployment.retailorder[count.index].invoke_url
+  overwrite = true
+}
+
+### Debugging Outputs
+output "retailorder_invoke_url" {
+  value =  formatlist(
+    "%s, %s", 
+    aws_api_gateway_rest_api.retailorder.*.name,
+    aws_ssm_parameter.retailorder_invoke_url.*.value
+  )
+}
