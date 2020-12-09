050d7dadd57a99ee53207e18495d76667acd7c2c
Geoff Higginbottom <github@geoffh.co.uk>
2020-12-09 13:44:24 +0000

    Cleaned up Debugging Outputs

--- api_gateway_retailorder.tf
+++ api_gateway_retailorder.tf
@@ -4,11 +4,11 @@ resource "aws_api_gateway_rest_api" "retailorder" {
   name        = "${element(var.function_ids, count.index)}_RetailOrder_api_gateway"
 }
 
-### Debugging Outputs
-output "retailorder_invoke_url" {
-  value =  formatlist(
-    "%s, %s", 
-    aws_api_gateway_rest_api.retailorder.*.name,
-    aws_api_gateway_deployment.retailorder.*.invoke_url
-  )
-}
+# ### Debugging Outputs
+# output "retailorder_invoke_url" {
+#   value =  formatlist(
+#     "%s, %s", 
+#     aws_api_gateway_rest_api.retailorder.*.name,
+#     aws_api_gateway_deployment.retailorder.*.invoke_url
+#   )
+# }
