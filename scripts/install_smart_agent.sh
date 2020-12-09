411a4b2364f9e466b4c1a7b94f385769e7fb7746
Geoff Higginbottom <github@geoffh.co.uk>
2020-12-09 12:07:39 +0000

    Update install_smart_agent.sh

--- scripts/install_smart_agent.sh
+++ scripts/install_smart_agent.sh
@@ -18,19 +18,7 @@ else
   printf "Realm Variable Detected ...\n"
 fi
 
-
 curl -sSL https://dl.signalfx.com/signalfx-agent.sh > /tmp/signalfx-agent.sh
-
-
-# if [ -z "$3" ] ; then
-#   printf "Version not specified, installing Latest version ...\n"
-#   sudo sh /tmp/signalfx-agent.sh --ingest-url https://ingest.$REALM.signalfx.com --api-url https://api.$REALM.signalfx.com $TOKEN
-#   exit 1
-# else
-#   printf "Version Variable Detected - Installing SignalFX Version $VERSION ..\n"
-#   sudo sh /tmp/signalfx-agent.sh --ingest-url https://ingest.$REALM.signalfx.com --api-url https://api.$REALM.signalfx.com $TOKEN --package-version $VERSION
-# fi
-
 
 if [ -z "$3" ] ; then
   printf "Version not specified, installing Latest version ...\n"
